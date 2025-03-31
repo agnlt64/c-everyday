4323b752a4ac5f89b23c91a8af7c24ebb363198e
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2025-03-31 22:41:16 +0200

    add more integer manipulation algorithms

--- int_manipulation/int_manipulation.c
+++ int_manipulation/int_manipulation.c
@@ -48,6 +48,50 @@ int bin_to_int(const char* str)
     return res;
 }
 
+int sum_of_digits(int n)
+{
+    if (n < 10)
+        return n;
+    return (n % 10) + sum_of_digits(n / 10);
+}
+
+int sum_of_digits_iter(int n)
+{
+    if (n < 10)
+        return n;
+
+    int res = n % 10;
+    while (n > 10)
+    {
+        n /= 10;
+        res += n % 10;
+    }
+    return res;
+}
+
+int reverse_internal(int n, int res)
+{
+    if (n == 0)
+        return res;
+    return reverse_internal(n / 10, res * 10 + n % 10);
+}
+
+int reverse(int n)
+{
+    return reverse_internal(n, 0);
+}
+
+int reverse_iter(int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        res = res * 10 + n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
 int main()
 {
     printf("%d\n", int_len(1));
@@ -60,6 +104,28 @@ int main()
 
     printf("bin(23) = %s\n", int_to_bin(23));
     printf("int(101101) = %d\n", bin_to_int("101101"));
+
+    int tests[5] = {
+        32145,
+        321,
+        9120,
+        4512,
+        669
+    };
+
+    printf("Sum of digits:\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        int val = tests[i];
+        printf("%d => (%d, %d)\n", val, sum_of_digits(val), sum_of_digits_iter(val));
+    }
+
+    printf("Reversed:\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        int val = tests[i];
+        printf("%d => (%d, %d)\n", val, reverse(val), reverse_iter(val));
+    }
     
     return 0;
 }
